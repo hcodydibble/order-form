923d21dd61dd64811db70939ba0910ee506915e9
allstot <n.allstot@gmail.com>
2017-08-24 15:32:44 -0700

    Added code for static pic and qty display and id

--- appcart.js
+++ appcart.js
@@ -1,25 +1,43 @@
 'use strict';
 var position1 = document.getElementById('col1');
-var position2 = document.getElementById('col2');
-var position3 = document.getElementById('col3');
+// var position2 = document.getElementById('col2');
+// var position3 = document.getElementById('col3');
 
-if (localStorage.getItem('selectCount')) {
-  var dataArray = JSON.parse(localStorage.getItem('selectCount'));
+var dataArray = ['img/bag.jpg', 12, 'img/breakfast.jpg', 1, 'img/pen.jpg', 5, 'img/tauntaun.jpg', 7];
+var picArray = ['img/bag.jpg', 'img/breakfast.jpg', 'img/pen.jpg', 'img/tauntaun.jpg',];
+var qtyArray = [12, 1, 5, 7];
 
-} else {
-  var dataArray = [];
-}
+// if (localStorage.getItem('selectCount')) {
+//   var dataArray = JSON.parse(localStorage.getItem('selectCount'));
+// } else {
+//   var dataArray = [];
+// }
 
 function renderCartPics () {
-  for (var itemCounter = 0; itemCounter < itemList.length; itemCounter ++){
+  for (var itemCounter = 0; itemCounter < picArray.length; itemCounter ++){
     var itemPic = document.createElement('img');
-    itemPic.setAttribute('src', itemName[newPicList[0]].fileNameLoc);
-    position1.appendChild(itemPic);
-    var itemQty = document.createElement('p');
-    itemQty.setAttribute('src', itemName[newPicList[0]].fileNameLoc);
-    position2.appendChild(itemQty);
+    var box = document.createElement('div');
+    var qty = document.createElement('p');
     var trashCan = document.createElement('img');
-    trashCan.setAttribute('src', itemName[newPicList[0]].fileNameLoc);
-    position3.appendChild(trashCan);
+    itemPic.setAttribute('src', picArray[itemCounter]);
+    trashCan.setAttribute('src', 'img/dragon.jpg');
+    box.appendChild(itemPic);
+    qty.innerText = 'Qty: ' + qtyArray[itemCounter];
+    box.appendChild(qty);
+    box.appendChild(trashCan);
+    position1.appendChild(box);
+    box.id = picArray[itemCounter];
   }
+  // for (var itemCounter = 0; itemCounter < picArray.length; itemCounter ++){
+  //   var itemQty = document.createElement('td');
+  //   itemQty.innerText = qtyArray[itemCounter];
+  //   row.appendChild(itemQty);
+  // }
+  // for (var itemCounter = 0; itemCounter < picArray.length; itemCounter ++){
+  //   var trashCan = document.createElement('img');
+  //   trashCan.setAttribute('src', 'img/dragon.jpg');
+  //   position1.appendChild(trashCan);
+  // }
 }
+
+renderCartPics();
