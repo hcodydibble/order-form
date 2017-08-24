7bc1a44b9f9f7801bde28011b5822f737062188f
Hewlette Dibble <hcodydibble@gmail.com>
2017-08-24 14:16:26 -0700

    form works and saves to local

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var orders = [];
+
+function Order(name,quantity){
+  this.name = name;
+  this.quantity = quantity;
+}
+
+function placeOrder(event){
+  event.preventDefault();
+  var newOrder = new Order();
+  newOrder.name = this.elements['products'].value;
+  newOrder.quantity = parseInt(this.elements['quantity'].value);
+  orders.push(newOrder);
+  localStorage.newOrders = JSON.stringify(orders);
+  dasForm.reset();
+}
+var dasForm = document.getElementById('dasForm');
+dasForm.addEventListener('submit',placeOrder);
